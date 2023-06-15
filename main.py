75332e0750f32c4dd15dee3b23a2b6f21b8a28ef
AEGamil <ahmedehabgamil@gmail.com>
2023-06-15 17:49:08 +0200

    handled the error when trying to select a champ right after accepting (but the user didnt fill the pick list) & cleaned  the code

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from utils import *
 import time
 
-# clientProps = getcluprops()
-# print(type(CHAMPS))
-
 res = req(type='GET',link='/lol-summoner/v1/current-summoner')
 sname = res.json().get('displayName','Summoner')
 
 
 print(f'Hi{PURPLE.format(sname)} i hope you have a nice game without trollers ;)')
-ban_input = input(f'\nEnter the name of the champion/s you want to{RED.format("ban")} (leave a dash "-" between them):\n')
+ban_input = input(f'\nEnter the name of the champion/s you want to{RED.format("ban")} (leave a hyphen "-" between them):\n')
 banList = validate(inputParser(ban_input))
-pick_input = input(f'\n\nEnter the name of the champion/s you want to{GREEN.format("pick")} (leave a dash "-" between them):\n')
+pick_input = input(f'\n\nEnter the name of the champion/s you want to{GREEN.format("pick")} (leave a hyphen "-" between them):\n')
 pickList = validate(inputParser(pick_input))
 print(RED.format('Your ban'),f'list consists of {len(banList)} : ',RED.format(banList))
 print(GREEN.format('Your pick') ,f'list consists of {len(pickList)} : ',GREEN.format(pickList),end='\n')
@@ -23,10 +20,9 @@ cellId = None
 while True:
 
 # updating the session so we can keep track of the games phase
-    # response =  requests.get(url=f'https://127.0.0.1:{port}/lol-gameflow/v1/gameflow-phase',auth=auth,data={},verify=False)
     response = req(type='GET',link='/lol-gameflow/v1/gameflow-phase')
     status = response.json()
-    print(status)
+    # print(status)
     
 # while the matchmaking phase to auto press ready
     if status == 'ReadyCheck':
@@ -40,10 +36,12 @@ while True:
     elif status == 'ChampSelect' and (len(banList) or len(pickList)):
         response = req(type='GET',link='/lol-champ-select/v1/session')
         champ_select_props = response.json()
-        # print(champ_select_props)
         if not cellId:     
             cellId, banId, pickId, banIndex, pickIndex = extractid(champ_select_props)
-            select(actorId=pickId,champ=pickList[0])
+            try:
+                select(actorId=pickId,champ=pickList[0])
+            except IndexError:
+                print('btw u didnt pick any champ')
         if champ_select_props.get('actions')[0][banIndex].get('isInProgress') and len(banList) and not banLock:
             # ban a champ
             print('u r in ban phase')
@@ -55,28 +53,11 @@ while True:
             print('u r in pick phase')
             pickLock = pick(actorId=pickId,pick_list=pickList)
             print()
-        # else:
-        #     select(actorId=pickId,champ=pickList[0])
-        # time.sleep(6)
     elif status == 'InProgress':
         sys.exit()
 
-    time.sleep(6)
+    time.sleep(8)
 
-# in champ select after we invoked the ids
-    # elif banId is not None:
-    #     response =  requests.post(url=f'https://127.0.0.1:{port}/lol-champ-select/v1/session/actions/18/complete',auth=auth,json= {'championId': 142},verify=False)
-    #     print(response.json())
-    #     time.sleep(4)
-        # response =  requests.post(url=f'https://127.0.0.1:{port}/lol-champ-select/v1/session/actions/{22}/complete',auth=auth,data={},verify=False)
-        # print(response.status_code)
-        # time.sleep(2)
-
-
-    # response =  requests.get(url=f'https://127.0.0.1:{port}/lol-gameflow/v1/gameflow-phase',auth=auth,data={},verify=False)
-    # x = response.json()
-    # print(x)
-    # time.sleep(1)
 
 # /lol-gameflow/v1/gameflow-phase  :get
 # /lol-champ-select/v1/session/actions/{id}    :patch
